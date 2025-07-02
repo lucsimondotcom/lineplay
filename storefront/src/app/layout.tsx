687bc5620a0e156feca01cb0cd2577f0fc3b165f
lucsimondotcom <contact@lucsimon.com>
2025-07-02 21:55:42 +0200

    GA

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -2,6 +2,7 @@ import { getBaseURL } from "@lib/util/env"
 import { Metadata } from "next"
 import "styles/globals.css"
 import LenisProvider from "../components/LenisProvider"
+import GoogleTagManager from "../components/GoogleTagManager"
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
@@ -11,6 +12,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
       <body>
+        <GoogleTagManager />
         <LenisProvider>
           <main className="relative">{props.children}</main>
         </LenisProvider>
